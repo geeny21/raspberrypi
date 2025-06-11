d2f058e10d2eca63af8575a453a8d7edc9347a9e
geeny21 <geeny21@daum.net>
2025-06-11 14:44:04 +0900

    i2c lcd 추가 25.06.11

---
+++ gpio/005_2_gpio_i2c_lcd2.py
@@ -0,0 +1,19 @@
+'''
+i2c Text LCD
+주소 : 0x27
+화면에 'hello' 출력 후 5초 대기
+'''
+from RPLCD.i2c import CharLCD
+import time
+
+# I2C 주소는 대부분 0x27 또는 0x3F (i2cdetect -y 1 명령어로 확인 가능)
+lcd = CharLCD('PCF8574', 0x27)
+
+lcd.write_string('hello')
+lcd.cursor_pos = (1, 0)  # 두 번째 줄의 첫 번째 위치로 커서를 이동
+lcd.write_string('goodbye')
+
+time.sleep(5)
+lcd.clear()  # LCD 화면을 지웁니다.
+lcd.close(clear=True)  # LCD를 닫고 화면을 지웁니다.
+
